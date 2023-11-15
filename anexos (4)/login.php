98b18a57b06d293f4920a09896202e85d37fb268
JoaoBauerv <136642016+JoaoBauerv@users.noreply.github.com>
2023-11-15 14:07:11 -0300

    Adicionando sesion, css pagina inicial

---
+++ anexos (4)/login.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'banco.php';
+
+session_start();
+
+$login = $_POST['login'];
+
+
+$sql = "SELECT * FROM  tb_pessoa WHERE nu_cpf = :login";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->bindParam(':login', $login, PDO::PARAM_STR);
+
+$stmt->execute();
+
+
+if ($stmt->fetchAll()) {
+    
+    $_SESSION['login'] = $login;
+    header('location:reciclagem2.php');
+} else {
+    unset($_SESSION['login']);
+    header('location:reciclagem.php');
+}
+
+?>
